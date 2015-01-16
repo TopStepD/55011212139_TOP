8d914fd756b160dc96b5fa95fb939c6247e9017d
TopStepD <toplcmore@hotmail.com>
2015-01-16 16:59:36 +0700

    array

---
+++ Array.playground/section-1.swift
@@ -0,0 +1,43 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var shoppingListExplicit = [String]()
+shoppingListExplicit=["Eggs","Milk"]
+//var shoppingListExplicit : [String] = ["Eggs","Milk"]
+
+var shoppingList = ["Eggs","Milk"]
+println("The Shopping list contains \(shoppingList.count) items.")
+shoppingList.append("Flour")
+
+shoppingList += ["Baking Powder"]
+
+shoppingList += ["Chocolate Spread" , "Cheese" , "Butter"]
+
+
+var firstItem = shoppingList [0]
+
+shoppingList [0] = "Six eggs"
+//shopping [0...3] = ["Bananas", "Apples"]
+shoppingList
+
+var airports: [String: String] = ["TY0" : "Tokyo", "DUB" : "Dublin"]
+
+if airports.isEmpty {
+    println("The airports dixtionary is empty.")
+}else {
+    println("The airports dixtionary is not empty.")
+}
+//prints "The airports dictionary is not empty."
+println("The airports dictionary contains \(airports.count)items.")
+//prints "The airports sictionary contains 2 item."
+
+airports ["LHR"] = "London"
+//the airports dictionary now contains 3 item
+
+airports ["LHR"] = "London Heathow"
+//the value for "LHR" has been changed to "london Heathrow"
+
+if let oldValue = airports.updateValue("Dublin International", forKey: "DUB"){
+    println(")
+}
---
+++ Frist.playground/section-1.swift
@@ -0,0 +1,25 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let tutorialTeam = 56
+let editorialTeam = 23
+var totalTeam = tutorialTeam + editorialTeam
+
+totalTeam += 1
+let priceIntIngerred = -19
+let priceIntExplicit:Int = -19
+
+let priceInferred = -19.99
+let priceExplicit:Double = -19.99
+
+let priceFloatInferred = -19.99
+let priceFloatExplicit:Float = -19.99
+
+let onSaleInferred = true
+let onSaleEcplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+
+
